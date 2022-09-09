3711502beee39ffe7154083a378f71483beed9e5
Duy Khanh <dzerok2020@gmail.com>
2022-09-09 14:40:07 +0700

    fix eslint and prettier

--- Freentship/prettier.config.js
+++ Freentship/prettier.config.js
@@ -1,11 +1,10 @@
 const options = {
-  "arrowParens": "avoid",
-  "singleQuote": true,
-  "bracketSpacing": true,
-  "endOfLine": "lf",
-  "semi": false,
-  "tabWidth": 2,
-  "trailingComma": "none",
+  arrowParens: 'avoid',
+  singleQuote: true,
+  backetSpacing: true,
+  endOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'none'
 }
-
 module.exports = options
